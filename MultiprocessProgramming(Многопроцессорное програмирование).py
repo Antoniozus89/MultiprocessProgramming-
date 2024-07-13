4ac46241fd92fc8ae5d05ad81168b685ee72a357
Antoniozus89 <166949414+Antoniozus89@users.noreply.github.com>
2024-07-13 15:15:15 +0300

    Add files via upload

---
+++ MultiprocessProgramming(Многопроцессорное програмирование).py
@@ -0,0 +1,51 @@
+import multiprocessing
+
+class WarehouseManager:
+
+    def __init__(self):
+        self.data = multiprocessing.Manager().dict()
+
+    def process_request(self, request):
+        product, action, quantity = request
+        if action == "receipt":
+            if product in self.data:
+                self.data[product] += quantity
+            else:
+                self.data[product] = quantity
+        elif action == "shipment":
+            if product in self.data and self.data[product] > 0:
+                self.data[product] -= quantity
+                if self.data[product] < 0:
+                    self.data[product] = 0
+
+    def run(self, requests):
+        processes = []
+        for request in requests:
+            p = multiprocessing.Process(target=self.process_request, args=(request,))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
+
+
+
+def main():
+    manager = WarehouseManager()
+
+    requests = [
+    ("product1", "receipt", 100),
+    ("product2", "receipt", 150),
+    ("product1", "shipment", 30),
+    ("product3", "receipt", 200),
+    ("product2", "shipment", 50)
+    ]
+
+    manager.run(requests)
+
+    print(manager.data)
+
+
+if __name__ == "__main__":
+    main()
